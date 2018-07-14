46c9c9cf3b5a99d696e1f37e829d40139a21a85f
github username <github email address>
2018-07-14 15:49:59 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |seasons, contestants|
+    if seasons == season
+      contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if attribute == "status" && value == "Winner"
+          return contestant["name"].split(" ").shift
+        end
+      end
+    end
+  end
+end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if occupation == value
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |seasons, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if hometown == value
+          counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if hometown == value
+          return contestant["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |seasons, contestants|
+    if season == seasons
+      ages = contestants.collect do |contestant|
+        contestant["age"].to_f
+      end
+    end
+  end
+age_average = ages.inject {|sum, element| sum + element} / ages.size
+age_average.round 
 end
