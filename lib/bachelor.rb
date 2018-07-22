05badf9eec57cba5b3967866080e4cf036110626
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-22 17:25:28 -0500

    done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_x, contestants_data|
+  	if season_x == season
+  		contestants_data.each do |status|
+  		if status["status"] == "Winner"
+  			return status["name"].split(" ")[0]
+  		end
+  	end 
+  	end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants_data|
+  	contestants_data.each do |occupation_search|
+  		if occupation_search["occupation"] == occupation 
+  			return occupation_search["name"]
+  		end
+  	end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  array = []
+  data.each do |season,contestants_data|
+  	contestants_data.each do |hometown_search|
+  		if hometown_search["hometown"] == hometown
+  			array << hometown
+  		end
+  	end
+  end
+  return array.length
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season,contestants_data|
+  	contestants_data.each do |hometown_search|
+  		if hometown_search["hometown"] == hometown
+  			return hometown_search["occupation"]
+  		end
+  	end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  array = [] 
+  count = 0
+  data.each do |season_x, contestants_data|
+  	if season_x == season
+  		contestants_data.each do |age|
+  			array << age["age"].to_f
+  			count +=1
+  		end
+  	end
+  	end
+  sum = 0
+  array.each do |i|
+  	sum +=i
+  end
+  return (sum / count).round
 end
